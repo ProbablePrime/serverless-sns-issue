2c7232dc2916dfaf26ad5f9c058fd2636655d3db
ProbablePrime <richard.fox@ahref.co.uk>
2017-07-12 00:25:56 +0200

    fixed

--- handler.js
+++ handler.js
@@ -1,16 +1,11 @@
 'use strict';
+const AWS = require('aws-sdk');
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  const sns = new AWS.SNS();
+  sns.publish({
+    Message: JSON.stringify({payload: 'Hello SNS'}),
+    MessageStructure: 'json',
+    TopicArn: process.env.SNS_TOPIC,
+  });
 };
